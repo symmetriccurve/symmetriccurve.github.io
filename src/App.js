b0263756f5b6519d56ac337a2a35653d4292f28a
Vikram Belde <belvikram@gmail.com>
2017-03-26 14:39:21 -0500

    removed scripts

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h1>Front End Developer</h1>
-        </div>
         <p className="App-intro">
           <h1 style={{fontWeight:'100'}}>Vikram Belde</h1>
         </p>
