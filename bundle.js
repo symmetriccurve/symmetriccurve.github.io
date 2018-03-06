eab7251997478c1fa05220f0f8cd2d89bab1e631
Vikram Belde <>
2018-03-05 23:54:57 -0600

    bundle updated

--- bundle.js
+++ bundle.js
@@ -13981,41 +13981,41 @@ var App = function (_React$Component) {
         name: 'Verizon - Sensity Systems (Project 2)',
         role: 'UI/UX Developer',
         location: 'Texas',
-        duration: 'December 2017 - Present',
+        duration: 'July 2017 - Present',
         fewWordsAboutCompany: 'Sensity® Systems uses energy-efficient LED lighting as the foundation for its IoT platform for Smart Cities. The company enables light owners to capitalize on the LED conversion process by embedding networking technology within both retrofit and new LED luminaires. With the Sensity open, privacy-by-design, multiservice NetSense™ platform, lighting owners are able to reduce energy costs and implement a variety of Smart City applications and services, including public safety, environmental and weather monitoring, parking management and location analytics.',
         fewWordsAboutProject: 'CMS(Content Management System) ',
         technologies: ['Reactjs', 'Redux', 'Webpack', 'Docker', 'Elastic Search'],
-        responsibilities: ["Designed and Implemented Content Management System portal from the scracth", "Extensivley worked on the Elastic Search including mappings and building queries", "Created Reusable Components to share across the platform and documented the usage", "Maintained Dev, Test and Prod environments using Docker Images", "Handled Versioning of the application, releases and documented the user guide", "Extensive usage of Redux to Manage the State", "implemeted node proxy to authenticate with the backend services"]
+        responsibilities: ["Designed and Implemented Content Management System portal from the scracth including the wireframes", "Extensivley worked on the Elastic Search including mappings and constructing queries", "Created Reusable Components to share across the CMS platform and documented the usage", "Maintained Dev, Test and Prod environments using Docker Images simultaneously deploying and maintaining the Instances", "Handled Versioning of the application, releases and documented the user guide", "Extensive usage of Redux to manage the state", "Implemeted node reverse proxy to forward UI requests to backend APIs", "Integrated ability to download the application state as a JSON file to ease bug fixing", "Implemented URL integration with Redux state and create URL from App state", "Added ability to download CSV file from user data selections", "Integrated React-Data-Grid to list the data and handle user selections", "Integrated Webpack to build and bundle the application and manage environment profiles"]
       }, {
         name: 'Verizon - Sensity Systems (Project 1)',
-        role: 'UI Developer',
+        role: 'Web Developer',
         location: 'California',
-        duration: 'January 2016 - June 2016',
-        description: 'Sensity Systems is Company by choice Sensity Systems is Company by choiceSensity Systems is Company by choiceSensity Systems is Company by choice',
-        technologies: ['React', 'jekins', 'React', 'jekins'],
-        responsibilities: ['Upgrade version of Reactjs, Nodejs of web application and dependent modules to latest versions.', 'Build and maintain web pages using HTML 5, CSS3, React-Bootstrap, SASS and React.js framework', 'Migrate existing functionality to new updated UI/UX with added features and functionalities.', 'Actively participated in code merges, releases and setting up continuous integration/continuous deployment using Jenkins', 'Managed multiple Environments spanning Develop, QA, Scaling and Security Handled and fixed multiple security vulnerabilities during penetrations testing(Qualys) and Performance testing.', 'Handled key components of the projects such as DataGrid, Scheduler, Google Maps(V3) and Data layer of the application', 'Contributed towards the Documentation of the product and release notes.', 'Interacted with users at different levels through Weekly demos to ensure continuous feedback and meet user requirements.', 'Developed and integrated functional test cases to test the user flows using protractor and generated test reports on each code push.', 'Introduced many third-party tools to increased the developer experience. Migrated bower components to respective npm modules effectively decrease the size of the code base', 'Introduced yarn to decrease the build time of the app and added ability to install modules in an isolated environment']
+        duration: 'January 2017 - June 2017',
+        fewWordsAboutCompany: 'From population growth to rapid urbanization, communities are facing new challenges to maintaining high standards of livability, resiliency and sustainability. Using innovative Internet of Things (IoT) and machine-to-machine (M2M) communication solutions, city executives can help improve efficiency and revolutionize cities to create a better quality of life for both work and play,Verizon working towards building cities, counties, sports venues, college campuses and corporate facilities that leverage technologies to better engage with citizens',
+        technologies: ['Reactjs', 'Jenkins', 'Docker', 'jQuery', 'Google Maps', 'Nodejs', 'Expressjs', 'Protractor', 'Enzyme', 'Chai', 'Mocha', 'Bower', 'Gulp'],
+        responsibilities: ['Upgrade version of Reactjs, Nodejs of web application and dependent modules to latest versions.', 'Build and maintain web pages using HTML 5, CSS3, React-Bootstrap, SASS and React.js framework', 'Migrate existing functionality to new updated UI/UX with added features and functionalities.', 'Actively participated in code merges, releases and setting up continuous integration/continuous deployment using Jenkins', 'Managed multiple Environments spanning Develop, QA, Scaling and Security Handled and fixed multiple security vulnerabilities during penetrations testing(Qualys) and Performance testing.', 'Handled key components of the projects such as DataGrid, Scheduler, Google Maps(V3) and Data layer of the application', 'Contributed towards the documentation of the product and release notes.', 'Interacted with users at different levels through Weekly demos to ensure continuous feedback and meet user requirements.', 'Developed and integrated functional test cases to test the user flows using protractor and generated test reports on each code push.', 'Introduced many third-party tools to increased the developer experience. Migrated bower components to respective npm modules effectively decrease the size of the code base', 'Introduced yarn to decrease the build time of the app and added ability to install modules in an isolated environment']
       }, {
         name: 'Verizon Wireless',
         role: 'Mobile App Developer - iOS/Android',
-        duration: 'November 2015 - December 2015',
+        duration: 'November 2015 - December 2016',
         location: 'Massachusetts',
-        description: 'Sensity Systems is Company by choice Sensity Systems is Company by choiceSensity Systems is Company by choiceSensity Systems is Company by choice',
-        technologies: ['React', 'jekins'],
+        fewWordsAboutCompany: 'From population growth to rapid urbanization, communities are facing new challenges to maintaining high standards of livability, resiliency and sustainability. Using innovative Internet of Things (IoT) and machine-to-machine (M2M) communication solutions, city executives can help improve efficiency and revolutionize cities to create a better quality of life for both work and play,Verizon working towards building cities, counties, sports venues, college campuses and corporate facilities that leverage technologies to better engage with citizens',
+        technologies: ['React-Native', 'Jest', 'Enzyme', 'Android', 'iOS', 'Objective C', 'Xcode'],
         responsibilities: ['Extensively worked on mobile app integration with Verizon ThingSpace Platform Developed Multiple mobile Application (Both iOS and Android) targeting IOT Platform using Facebook’s React Native', 'Framework that has ability to control IOT devices.', 'Used Google’s Firebase as backend, data querying and User Authentication. Maintained Developer certificates and provisioning certificates and app installs on multiple devices', 'Set up Live reloads on multiple devices to ease the development on multiple devices to check look and feel of the UI during development.', 'Implemented iOS Push Notification feature using Urban Airship as a third party provider', 'Implemented JWT authentication for User Authentication and added role based UI responsiveness', 'Extensively worked on Platform Maps to render markers (Devices Deployed), polylines(Routes) and polygons(Device Awareness) using Geo-fence Coordinates', 'Build a independent module(NPM module) around the requirement and added capabilities to be used across multiple app builds', 'Implemented Flux and Redux architecture to maintain state of the Applications Worked on apps namely iPair- Pairing IOT devices, CoHo – Control Home , CityWise – Control City wide IOT devices and AgTech – Control Agricultural Devices.', 'Integrated live video stream using react-native- video for iOS platform and handled error cases.', 'Collaborated with design team, QAs, scrum master and product owner to implement enhancements or new applications during daily stand-ups, sprint planning and sprint retrospectives.']
       }, {
         name: 'BM Tech Solutions',
         role: 'Mobile App Developer - iOS',
         location: 'Ohio',
         duration: 'August 2015 - October 2015',
-        description: 'Sensity Systems is Company by choice Sensity Systems is Company by choiceSensity Systems is Company by choiceSensity Systems is Company by choice',
-        technologies: ['React', 'jekins'],
+        fewWordsAboutCompany: 'BM Tech Solutions is a technology and IT consulting firm and specialize in providing cost effective IT Solutions to  variety of customers. BM Tech Solutions is ready to assists in every phase of the project lifecycle from planning through execution as well as providing on-going support',
+        technologies: ['iOS', 'jekins'],
         responsibilities: ['Develop and maintain an employee management iOS app', 'Add additional functionalities like secure login(oauth) and push notification.', 'Integration with third-party user authentication and login (linkedin, google) Enhance UI and fix issues with multiple device compatibility', 'Document the development process and steps to set-up the environment']
       }, {
         name: 'Manpower Solutions',
-        role: 'UI Developer - iOS',
+        role: 'Web Developer',
         location: 'India',
-        duration: 'November 2015 - December 2015',
-        description: 'Sensity Systems is Company by choice Sensity Systems is Company by choiceSensity Systems is Company by choiceSensity Systems is Company by choice',
+        duration: 'April 2013 - December 2013',
+        fewWordsAboutCompany: 'Sensity Systems is Company by choice Sensity Systems is Company by choiceSensity Systems is Company by choiceSensity Systems is Company by choice',
         technologies: ['React', 'jekins'],
         responsibilities: ['Designed and developed employee portal from the scratch', 'Implemented functionalities to add/update/Delete employee documents online', 'Added functionality to download employee records in multiple formats(PDF/doc)', 'Implemented ability to upload CSV files and populate the forms on the portal Added widgets to ease the employee login and logout procedures']
       }];
@@ -14028,13 +14028,24 @@ var App = function (_React$Component) {
         _react2.default.createElement(
           'div',
           { style: { padding: '5%', marginLeft: '3%', marginRight: '3%', backgroundColor: 'white' } },
+          _react2.default.createElement('hr', null),
           _react2.default.createElement(
             'div',
             { className: 'extraExtralarge-text' },
             ' VIKRAM BELDE - UI/UX Developer'
           ),
+          _react2.default.createElement(
+            'div',
+            { className: 'large-text' },
+            'Enthuastic Javascript developer cutting through the technology and learning to the level of dust.'
+          ),
+          _react2.default.createElement('hr', null),
           projects.map(function (eachProject) {
-            return _react2.default.createElement(_Project2.default, { projectInfo: eachProject });
+            return _react2.default.createElement(
+              'div',
+              null,
+              _react2.default.createElement(_Project2.default, { projectInfo: eachProject })
+            );
           })
         )
       );
@@ -14212,7 +14223,7 @@ var Project = function (_React$Component) {
     value: function render() {
       return _react2.default.createElement(
         'div',
-        { style: { padding: '3%', marginTop: '3%', boxShadow: '0 2px 10px 0 rgba(0, 0, 0, 0.16' } },
+        { style: { padding: '2%', marginTop: '3%', boxShadow: '0 2px 10px 0 rgba(0, 0, 0, 0.16' } },
         _react2.default.createElement(
           'div',
           { className: 'row' },
@@ -14255,7 +14266,7 @@ var Project = function (_React$Component) {
         _react2.default.createElement('hr', null),
         _react2.default.createElement(
           'div',
-          { style: { margin: '1%' } },
+          { style: { padding: '2px' } },
           _react2.default.createElement(
             'ul',
             { className: 'normal-text' },
